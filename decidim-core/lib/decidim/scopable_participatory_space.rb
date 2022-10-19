e2b2570c365a49badc3f58f396834dcf03f69191
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-10-19 13:12:48 +0200

    Make mandatory association with scope in scopable concerns

--- decidim-core/lib/decidim/scopable_participatory_space.rb
+++ decidim-core/lib/decidim/scopable_participatory_space.rb
@@ -13,7 +13,7 @@ module Decidim
       belongs_to :scope,
                  foreign_key: "decidim_scope_id",
                  class_name: "Decidim::Scope",
-                 optional: true
+                 optional: false
     end
   end
 end

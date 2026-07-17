0209cc41d605a44010241457e637dfc0abeea8df
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2026-07-17 19:32:12 +0200

    Fix conditioned_questions association of Question
    has_many through ignores the foreign_key attribute and a source is
    required because association name on the join model can't be
    inferred from the association name

--- decidim-forms/app/models/decidim/forms/question.rb
+++ decidim-forms/app/models/decidim/forms/question.rb
@@ -44,7 +44,7 @@ module Decidim
       # Questions which have display conditions based on the value of this question's answer
       has_many :conditioned_questions,
                through: :display_conditions_for_other_questions,
-               foreign_key: "decidim_condition_question_id",
+               source: :question,
                class_name: "Question"
 
       has_many :answers,
